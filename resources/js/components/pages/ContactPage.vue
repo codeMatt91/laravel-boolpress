94fbc52da878cb9e1de45ea42897a1b3492da795
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-04-05 18:14:33 +0200

    added 2 components : Homepage + contact

---
+++ resources/js/components/pages/ContactPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2>Contatti</h2>
+      <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat enim sapiente cumque exercitationem necessitatibus, asperiores repellat veritatis magni nihil assumenda laboriosam vel, a voluptatum, sed praesentium nobis architecto non. Corrupti?
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "ContactPage",
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <ViewPosts />
+  </div>
+</template>
+
+<script>
+import ViewPosts from "../Posts/ViewPosts.vue";
+export default {
+name: "HomePage",
+components: {
+    ViewPosts,
+}
+}
+</script>
+
+<style>
+
+</style>
