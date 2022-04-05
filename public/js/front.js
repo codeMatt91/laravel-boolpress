14cc7c43c89593bdb87fed961f3b25ab68af15a7
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-04-05 19:09:29 +0200

    added button to caming back from page 404

--- public/js/front.js
+++ public/js/front.js
@@ -2137,6 +2137,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "NotFoundPage"
 });
@@ -38738,27 +38739,33 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    {
+      staticClass:
+        "container d-flex flex-column align-items-center justify-content-center",
+    },
+    [
+      _c("h3", { staticClass: "text-muted" }, [
+        _vm._v("404 | Pagina non trovata"),
+      ]),
+      _vm._v(" "),
+      _c(
+        "div",
+        {
+          staticClass: "btn btn-secondary",
+          on: {
+            click: function ($event) {
+              return _vm.$router.back()
+            },
+          },
+        },
+        [_vm._v("Indietro")]
+      ),
+    ]
+  )
 }
-var staticRenderFns = [
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c(
-      "div",
-      {
-        staticClass:
-          "container d-flex align-items-center justify-content-center",
-      },
-      [
-        _c("h3", { staticClass: "text-muted" }, [
-          _vm._v("404 | Pagina non trovata"),
-        ]),
-      ]
-    )
-  },
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/components/pages/NotFoundPage.vue
+++ resources/js/components/pages/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="container d-flex align-items-center justify-content-center">
+  <div class="container d-flex flex-column align-items-center justify-content-center">
       <h3 class="text-muted">404 | Pagina non trovata</h3>
+      <div class="btn btn-secondary" @click="$router.back()">Indietro</div>
   </div>
 </template>
 
